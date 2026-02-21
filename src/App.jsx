d6719e945d4ac25995635becf79a559b9ef9490a
chebuo <bob.chebuo@gmail.com>
2026-02-22 00:26:46 +0900

    [fix] divの位置

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <>
     <div className="slider">
+      <div>
       <a 
         href="https://ja.wikipedia.org/wiki/%E3%83%81%E3%82%A7%E3%83%96%E3%83%A9%E3%83%BC%E3%82%B7%E3%82%AB"
         target="_blank"
@@ -17,8 +18,9 @@ function App() {
             <img key={i} src={chebu} alt="chebu" className="slide-img" />
           ))}
       </a>
-      <h1>chebuoのポートフォリオ</h1>
+      </div>
     </div>
+      <h1>chebuoのポートフォリオ</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
