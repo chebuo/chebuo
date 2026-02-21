19d68896d6f133400067ba9e1974df18ba90e322
chebuo <bob.chebuo@gmail.com>
2026-02-22 00:03:57 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import chebu from '/assets/chebu_fine.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <div>
+      <a href="https://ja.wikipedia.org/wiki/%E3%83%81%E3%82%A7%E3%83%96%E3%83%A9%E3%83%BC%E3%82%B7%E3%82%AB">
+        <img src={chebu} alt="chebu" />
+      </a>
       <h1>chebuoのポートフォリオ</h1>
+    </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         tomatotabetai
