ef7b7809efc9cecfe2666b43cb1b68889c3ffc13
chebuo <bob.chebuo@gmail.com>
2026-02-22 00:14:23 +0900

    [fix] path指定の間違い

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import chebu from '/assets/chebu_fine.jpg'
+import chebu from 'assets/chebu_fine.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
