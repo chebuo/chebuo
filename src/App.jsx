0aa79b23b1d1e79006f6abbfeea832b13021319e
chebuo <bob.chebuo@gmail.com>
2026-02-22 16:18:41 +0900

    [add] 画像を横スクロール、SNSへのリンク、テキスト等々

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,93 @@
-import { useState } from 'react'
+import { useRef, useState ,useEffect} from 'react'
 import './App.css'
+import History from './history.jsx'
 import chebu from './assets/chebu_fine.jpg'
+import chebu_yellow from './assets/yellow_chebu.jpg'
+import github from './assets/github_icon.png'
+import x from './assets/x_icon.png'
+import unityroom from './assets/unityroom_icon.jpg'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const trackRef=useRef(null);
+  const [trackWidth,setTrackWidth]=useState(0);
+  useEffect(()=>{
+    const updateWidth=()=>{
+    if(trackRef.current)setTrackWidth(trackRef.current.scrollWidth/2);
+    }
+    updateWidth();
+    window.addEventListener('resize', updateWidth);
+    window.addEventListener('load', updateWidth);
+    return () =>{
+      window.removeEventListener('load', updateWidth);
+      window.removeEventListener('resize',updateWidth)
+    }
+  },[]);
 
+  const slides = [
+  {
+    img: chebu,
+    link: "https://ja.wikipedia.org/wiki/%E3%83%81%E3%82%A7%E3%83%96%E3%83%A9%E3%83%BC%E3%82%B7%E3%82%AB"
+  },
+  {
+    img: chebu_yellow,
+    link: "https://dash.cloudflare.com/e6ab8471d01bc4780a69c6978ce2f252/pages/view/chebuo"
+  }
+];
+  const repeatSlides=Array(12).fill(slides).flat();
+  const loopSlides=[...repeatSlides, ...repeatSlides];
   return (
     <>
     <div className="slider">
-      <div className="slide-track">
-      {Array.from({ length: 8 }).map((_, i) => (
+      <div 
+      ref={trackRef}
+      className="slide-track"
+      style={{animation:trackWidth?`scroll ${(trackWidth*3)/1}s linear infinite`:'none'}}
+      >
+      {loopSlides.map((slide, i) => (
       <a 
         key={i}
-        href="https://ja.wikipedia.org/wiki/%E3%83%81%E3%82%A7%E3%83%96%E3%83%A9%E3%83%BC%E3%82%B7%E3%82%AB"
+        href={slide.link}
         target="_blank"
         rel="noopener noreferrer"
         >
-          <img src={chebu} alt="chebu" className="slide-img" />     
+          <img src={slide.img} alt={i % 2 === 0 ? "chebu" : "chebu_yellow"} className="slide-img" />     
         </a>
       ))}
       </div>
     </div>
-      <h1>chebuoのポートフォリオ</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <h1>chebuoのポートフォリオへようこそ</h1>
+      <h2>SNSアカウント一覧</h2>
+      <div className="iconList">
+        <a 
+        href="https://github.com/chebuo"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+          <img src={github} alt="github_icon" className="icon"/>
+          <p>github</p>
+        </a>
+        <a 
+        href="https://x.com/chebuo_"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+          <img src={x} alt="x_icon" className="icon"/>
+          <p>X</p>
+        </a>
+        <a 
+        href="https://unityroom.com/users/chebuo"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+          <img src={unityroom} alt="unityroom_icon" className="icon"/>
+          <p>UnityRoom</p>
+        </a>
       </div>
-      <p className="read-the-docs">
-        tomatotabetai
-      </p>
+      <h2>成果物一覧</h2>
+      <button onClick={()=>{window.location.href="/history.jsx"}}>
+        <h2>活動記録をみる</h2>
+      </button>
+
     </>
   )
 }
