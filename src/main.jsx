221cd7adcd54f7dd4a06d53dab3011a12360cd01
chebuo <bob.chebuo@gmail.com>
2026-02-24 11:40:21 +0900

    [add] analytics追加してみた

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
+import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
+    <Analytics/>
   </BrowserRouter>
 )
